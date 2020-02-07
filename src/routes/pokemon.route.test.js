6669f7fc3a1d118992a258522a2af599ea33a40d
Teo Jin Cheng <jinchengproject@gmail.com>
2020-02-07 16:52:10 +0800

    add testing

---
+++ src/routes/pokemon.route.test.js
@@ -0,0 +1,76 @@
+const request = require("supertest");
+const app = require("../../src/app");
+const Pokemon = require("../../src/models/new-pokemon.model");
+const mongoose = require("mongoose");
+const { MongoMemoryServer } = require("mongodb-memory-server");
+
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+mongoose.set("useUnifiedTopology", true);
+
+describe("pokemons", () => {
+  let mongoServer;
+  beforeAll(async () => {
+    try {
+      mongoServer = new MongoMemoryServer();
+      const mongoUri = await mongoServer.getConnectionString();
+      await mongoose.connect(mongoUri);
+    } catch (err) {
+      console.error(err);
+    }
+  });
+
+  afterAll(async () => {
+    await mongoose.disconnect();
+    await mongoServer.stop();
+  });
+
+  beforeEach(async () => {
+    const pokemonData = [
+      {
+        id: 1,
+        name: "Pikachu",
+        japaneseName: "ピカチュウ",
+        baseHP: 35,
+        category: "Mouse Pokemon"
+      },
+      {
+        id: 2,
+        name: "Squirtle",
+        japaneseName: "ゼニガメ",
+        baseHP: 44,
+        category: "Tiny Turtle Pokemon"
+      }
+    ];
+    await Pokemon.create(pokemonData);
+  });
+
+  afterEach(async () => {
+    await Pokemon.deleteMany();
+  });
+
+  it("GET should respond with all pokemons", async () => {
+    const expectedPokemonData = [
+      {
+        id: 1,
+        name: "Pikachu",
+        japaneseName: "ピカチュウ",
+        baseHP: 35,
+        category: "Mouse Pokemon"
+      },
+      {
+        id: 2,
+        name: "Squirtle",
+        japaneseName: "ゼニガメ",
+        baseHP: 44,
+        category: "Tiny Turtle Pokemon"
+      }
+    ];
+
+    const { body: actualPokemons } = await request(app)
+      .get("/pokemons")
+      .expect(200);
+    expect(actualPokemons).toMatchObject(expectedPokemonData);
+  });
+});
