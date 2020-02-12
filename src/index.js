76b4b90b8e3607456ec5bbd6ea63f1b2ae011580
Teo Jin Cheng <jinchengproject@gmail.com>
2020-02-12 13:47:52 +0800

    update the index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 require("./utils/db");
 const app = require("./app");
 const PORT = 3000;
-
-const server = app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}...`);
+const server = app.listen(process.env.PORT || PORT, () => {
+  console.log(`Express app started on http://localhost:${PORT}`);
 });
